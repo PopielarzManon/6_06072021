a1356bf916e4a3740ac1f59a0558d2000ffb6980
PopielarzManon <popielarzmanon@gmail.com>
2021-08-03 11:58:28 +0200

    :construction: Mise à jour

---
+++ backend/models/Sauce.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const thingSchema = mongoose.Schema({
+  
+  
+  // id: { type: String, required: true },
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  usersLiked: { type: Array, required: true },
+  userDisliked: { type: Array, required: true },
+});
+
+module.exports = mongoose.model('Sauce', thingSchema);
