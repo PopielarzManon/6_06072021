6746cb21259919286166dc891e738b6f65e89c99
PopielarzManon <popielarzmanon@gmail.com>
2021-08-22 18:49:22 +0200

    :lock: Passvalidator

--- backend/middleware/pass.js
+++ backend/middleware/pass.js
@@ -3,7 +3,7 @@ const passSchema = require("../models/password");
 module.exports = (req, res, next) => {
     if (!passSchema.validate(req.body.password)) {
         return res.status(400).json({
-            error: "Veuillez utiliser un mot de passe fort. Au moins une minuscule et majuscule, entre 8 dont au moins 2 chiffres" +
+            error: "Mot de passe faible. Au moins une minuscule et majuscule, entre 8 dont au moins 2 chiffres" +
                 passSchema.validate("Essaie encore", { list: true }),
         });
     } else {
--- backend/models/password.js
+++ backend/models/password.js
@@ -2,20 +2,12 @@ const passValidator = require("password-validator");
 
 const passSchema = new passValidator();
 passSchema
-    .is()
-    .min(8)
-    .has()
-    .uppercase()
-    .has()
-    .lowercase()
-    .has()
-    .digits(2)
-    .has()
-    .not()
-    .spaces()
-    .is()
-    .not()
-    .oneOf(["Passw0rd", "Password123"]);
+    .is().min(8)
+    .has().uppercase()
+    .has().lowercase()
+    .has().digits(2)
+    .has().not().spaces()
+    .is().not().oneOf(["Passw0rd", "Password123"]);
 
 
 module.exports = passSchema;
